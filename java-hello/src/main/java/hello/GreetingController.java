9158aabce028eb193ecedb1d7147325c9422134e
Alberto Gómez <albgomceb@alum.us.es>
2017-11-23 19:13:56 +0100

    Cambios a GrettinController

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController {
     }
 
     @RequestMapping("/bye")
-    public String bye(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String bye(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
         model.addAttribute("name", name);
         return "bye";
     }
