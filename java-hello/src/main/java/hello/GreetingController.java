aad4cb090d20582d29995b23c66aafef5e412307
Alberto Gómez <albgomceb@alum.us.es>
2017-11-23 19:07:38 +0100

    Añadido metodo a la clase GreetingController

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -14,4 +14,10 @@ public class GreetingController {
         return "greeting";
     }
 
+    @RequestMapping("/bye")
+    public String bye(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "bye";
+    }
+
 }
